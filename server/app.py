47cda907b2d5058f1fec7fe21a72ab414370e134
wachuka7 <rachaelwachuka7@gmail.com>
2024-04-15 13:26:24 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -17,7 +17,35 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
 
+        if user:
+        
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
+    
+class Logout(Resource):
+    def delete(self):
+
+        session['user_id'] = None
+        
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        
+        return {}, 401
+    
 class ClearSession(Resource):
 
     def delete(self):
@@ -51,7 +79,9 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
